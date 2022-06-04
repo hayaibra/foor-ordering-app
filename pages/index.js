687669365cdd5834422e08b98dea5546f4e3442e
hayaIbra <hayaibra217@gmail.com>
2022-06-04 15:20:03 +0300

    edit 3

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,35 @@ import { useState } from "react";
 export default function Home() {
   const [products, setProducts] = useState([]);
 
+  function fetchWithAutoRetry(fetcher, maxRetryCount) {
+    return new Promise((resolve, reject) => {
+      let retries = 0;
+      const caller = () =>
+        fetcher()
+          .then((data) => {
+            resolve(data);
+          })
+          .catch((error) => {
+            if (retries < maxRetryCount) {
+              retries++;
+              caller();
+            } else {
+              reject(error);
+            }
+          });
+      retries = 1;
+      caller();
+    });
+  }
+
   const fetchproducts = async () => {
     const response = await fetch("/api/products");
     const data = await response.json();
     setProducts(data);
+    return data;
   };
 
-  fetchproducts();
+  fetchWithAutoRetry(fetchproducts, 5);
 
   return (
     <div>
